9eb0f84e820f76a73f9042165e02c60fc1e93da3
Growence Dev <dev@growence.media>
2026-03-14 00:51:35 +0000

    feat: Add WhatsApp floating button (GetButton.io widget)
    - Position: left (RTL site)
    - Number: +966571373367
    - Call to action: تواصل معنا

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,6 +35,23 @@ export default function RootLayout({
           }}
         />
         {children}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function () {
+                var options = {
+                  whatsapp: "966571373367",
+                  call_to_action: "تواصل معنا",
+                  position: "left",
+                };
+                var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+                var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+                s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+                var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
